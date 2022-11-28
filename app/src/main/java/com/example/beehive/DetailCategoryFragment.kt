b3e1d58c1821f89f6015705690afff1430aed733
calvinadhikang <calvinadhikang@gmail.com>
2022-11-29 00:37:53 +0700

    adding DetailCategoryFragment, DetailStingFragment, and LandingPageFragment

---
+++ app/src/main/java/com/example/beehive/DetailCategoryFragment.kt
@@ -0,0 +1,23 @@
+package com.example.beehive
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class DetailCategoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
