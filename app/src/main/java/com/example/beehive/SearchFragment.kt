51fe7468e56f19573c1bca38a76a40c3381b0721
calvinadhikang <calvinadhikang@gmail.com>
2022-11-29 12:10:27 +0700

    Adding SearchFragment, DetailFragment, and Lorem Ipsum

--- app/src/main/java/com/example/beehive/SearchFragment.kt
+++ app/src/main/java/com/example/beehive/SearchFragment.kt
@@ -6,15 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-class DetailBuyStingFragment : Fragment() {
-
+class SearchFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_buy_sting, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
+
 }
