0a608b2f1f36ed9a8cf2cada7a2f564bd64958d2
Michael Kevin <michaelkevinwijaya257@gmail.com>
2022-11-29 11:07:08 +0700

    Detail and Buy Sting

--- app/src/main/java/com/example/beehive/DetailBuyStingFragment.kt
+++ app/src/main/java/com/example/beehive/DetailBuyStingFragment.kt
@@ -34,7 +34,7 @@ class DetailStingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_sting, container, false)
+        return inflater.inflate(R.layout.fragment_detail_buy_sting, container, false)
     }
 
     companion object {
