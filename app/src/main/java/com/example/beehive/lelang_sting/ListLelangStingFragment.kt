62a879df8003ebc86c41de7c9b951cabffe31b05
Enrico <enricoadi49@gmail.com>
2023-01-06 11:35:23 +0700

    List lelang sting layout done

--- app/src/main/java/com/example/beehive/lelang_sting/ListLelangStingFragment.kt
+++ app/src/main/java/com/example/beehive/lelang_sting/ListLelangStingFragment.kt
@@ -1,14 +1,24 @@
 package com.example.beehive.lelang_sting
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.beehive.activities.MainActivity
 import com.example.beehive.R
+import com.example.beehive.api_config.ApiConfiguration
+import com.example.beehive.data.BasicDRO
+import com.example.beehive.data.DeclineTransactionStingDTO
+import com.example.beehive.data.LelangStingData
+import com.example.beehive.data.ListLelangStingDRO
+import com.example.beehive.transaction_sting.RVComplainAdapter
+import retrofit2.Call
+import retrofit2.Callback
 
 class ListLelangStingFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +44,42 @@ class ListLelangStingFragment : Fragment() {
 
         val btnNavCreateLelangSting: Button = view.findViewById(R.id.btnNavCreateLelangSting)
         val rvListLelangSting: RecyclerView = view.findViewById(R.id.rvListLelangSting)
+
+        lateinit var adapter: RVListLelangSting
+
+        val client = ApiConfiguration.getApiService().fetchLelangSting(
+            remember_token = acti.userLogin!!.REMEMBER_TOKEN!!,
+        )
+        client.enqueue(object: Callback<ListLelangStingDRO> {
+            override fun onResponse(call: Call<ListLelangStingDRO>, response: retrofit2.Response<ListLelangStingDRO>){
+                if(response.isSuccessful){
+                    val responseBody = response.body()
+                    if(responseBody!=null){
+                        if(responseBody.data!=null){
+                            val listLelang:List<LelangStingData> = (responseBody.data as List<LelangStingData>?)!!
+                            adapter = RVListLelangSting(listLelang)
+                            rvListLelangSting.adapter = adapter
+                            rvListLelangSting.layoutManager =
+                                GridLayoutManager(requireContext(), 1, GridLayoutManager.VERTICAL, false)
+                        }
+                    }
+                }
+                else{
+                    val statusCode:Int = response.code()
+                    Log.e("ERROR FETCH", "Fail Access: $statusCode")
+                    if(statusCode==401){
+                        acti.showModal("Unauthorized"){}
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ListLelangStingDRO>, t: Throwable) {
+                Log.e("ERROR FETCH", "onFailure: ${t.message}")
+                acti.showModal("Poor network connection detected"){}
+            }
+
+        })
+
         btnNavCreateLelangSting.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.frMain, CreateLelangStingFragment())
---
+++ app/src/main/java/com/example/beehive/lelang_sting/RVListLelangSting.kt
@@ -0,0 +1,53 @@
+package com.example.beehive.lelang_sting
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.beehive.CurrencyUtils.toRupiah
+import com.example.beehive.R
+import com.example.beehive.data.ComplainData
+import com.example.beehive.data.LelangStingData
+import com.example.beehive.transaction_sting.RVComplainAdapter
+
+class RVListLelangSting (
+    var data: List<LelangStingData>
+) : RecyclerView.Adapter<RVListLelangSting.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val lblStatus: TextView = view.findViewById(R.id.lblStatus)
+        val lblHarga: TextView = view.findViewById(R.id.lblHarga)
+        val lblDeskripsi: TextView = view.findViewById(R.id.lblDeskripsi)
+        val lblTanggal: TextView = view.findViewById(R.id.lblTanggal)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_list_lelang, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var obj = data[position]
+        when(obj.STATUS){
+           " -1"-> holder.itemView.setBackgroundResource(R.drawable.background_pink_curved_full)
+            "0"-> holder.itemView.setBackgroundResource(R.drawable.background_blue_curved_full)
+            "1"-> holder.itemView.setBackgroundResource(R.drawable.background_orange_curved_full)
+            "2"-> holder.itemView.setBackgroundResource(R.drawable.background_lightgreen_curved_full)
+        }
+        holder.lblStatus.text = obj.statusString
+        holder.lblHarga.text = obj.COMMISION!!.toInt().toRupiah()
+        holder.lblTanggal.text = obj.CREATED_AT
+        var requirement:String = obj.REQUIREMENT_PROJECT!!
+        if(requirement.length>50){
+            requirement = requirement.substring(0,49)+"..."
+        }
+        holder.lblDeskripsi.text = requirement
+
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
